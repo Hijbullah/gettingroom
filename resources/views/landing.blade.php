6fc2da2211634b2ca4ac50e215fdbd612d0315f0
Hijbullah Muhamed Kyrul Amin <hijbullaah@gmail.com>
2020-01-11 20:57:08 +0600

    home page responsive

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -331,7 +331,7 @@
         </div>       
     </section>
     <section class="gr-services bg-light">
-        <div class="container-fluid">
+        <div class="container">
             <div class="row">
                 <div class="col-md-4">
                     <div class="single-box">
